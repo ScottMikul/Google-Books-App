8d072008ed144cf749b5ec150028a54366037b54
Scotty <Scott.p.mikul@gmail.com>
2020-06-03 12:16:58 -0400

    added some validation to make sure that the google books api gives back good data for the searches.

--- client/src/Main.js
+++ client/src/Main.js
@@ -16,39 +16,43 @@ export function Search() {
         event.preventDefault();
         searchGoogleBooks();
     }
-    
+
     function searchGoogleBooks() {
-        
+
         console.log("rendering seearch lol");
         axios.get("https://www.googleapis.com/books/v1/volumes?q=\"" + searchText + "\"").then(
             res => {
-                const booksArr = res.data.items.slice(0, 5).map(
-                    item => {
+                const booksArr = res.data.items.slice(0, 5).reduce(
+                    (resultArr, item) => {
                         let thumbnail;
-                        if (item.volumeInfo.imageLinks) {
+                        if (item.volumeInfo.imageLinks && item.volumeInfo.title && item.volumeInfo.authors && item.volumeInfo.description && item.volumeInfo.infoLink) {
                             thumbnail = <img src={item.volumeInfo.imageLinks.thumbnail}></img>
+                            resultArr.push(
+                                <div className="card m-2">
+                                    <div className="card-body col-6">
+                                        {thumbnail}
+                                        <h4>{item.volumeInfo.title}</h4>
+                                        <div>{item.volumeInfo.authors}</div>
+                                        <hr></hr>
+                                        <div>{item.volumeInfo.description}</div>
+                                        <a href={item.volumeInfo.infoLink}>Link To page</a>
+                                        <div></div>
+                                        <div className="btn btn-primary" onClick={() => API.saveBook({
+                                            thumbnail: item.volumeInfo.imageLinks.thumbnail, title: item.volumeInfo.title,
+                                            authors: item.volumeInfo.authors, description: item.volumeInfo.description, link: item.volumeInfo.infoLink
+                                        })}>
+                                            Save
+                                    </div>
+                                    </div>
+    
+                                </div>
+                            )
                         }
-                        console.log({thumbnail:item.volumeInfo.imageLinks.thumbnail, title:item.volumeInfo.title,
-                            authors:item.volumeInfo.authors,description:item.volumeInfo.description, link:item.volumeInfo.infoLink});
-                        return (
-                            <div className="card m-2">
-                                <div className="card-body col-6">
-                                {thumbnail}                                
-                                <h4>{item.volumeInfo.title}</h4>
-                                <div>{item.volumeInfo.authors}</div>
-                                <hr></hr>                    
-                                <div>{item.volumeInfo.description}</div>
-                                <a href={item.volumeInfo.infoLink}>Link To page</a>
-                                <div></div>
-                                <div className="btn btn-primary" onClick={()=>API.saveBook({thumbnail:item.volumeInfo.imageLinks.thumbnail, title:item.volumeInfo.title,
-                                    authors:item.volumeInfo.authors,description:item.volumeInfo.description, link:item.volumeInfo.infoLink})}>
-                                        Save
-                                </div>
-                                </div>
 
-                            </div>
-                        )
-                    }
+                        return resultArr;
+
+
+                    }, []
                 )
                 setBooks(booksArr);
             }
@@ -60,16 +64,16 @@ export function Search() {
         <div>
 
 
-            <form onSubmit={(event)=>handleFormSubmit(event)}>
+            <form onSubmit={(event) => handleFormSubmit(event)}>
 
                 <div className="input-group mb-3">
                     <div className="input-group-prepend">
-                        <button className="btn btn-outline-secondary" type="button"  onClick={(event) => handleFormSubmit(event)}>Search</button>
+                        <button className="btn btn-outline-secondary" type="button" onClick={(event) => handleFormSubmit(event)}>Search</button>
                     </div>
-                    <input type="text" className="form-control form-control-lg"  placeholder="e.g. javascript" value={searchText} onChange={(event)=> setSearchText(event.target.value)} />
+                    <input type="text" className="form-control form-control-lg" placeholder="e.g. javascript" value={searchText} onChange={(event) => setSearchText(event.target.value)} />
                 </div>
             </form>
-                {books}
+            {books}
         </div>)
 }
 
@@ -77,42 +81,42 @@ export function Search() {
 export function Saved() {
     const [books, setBooks] = useState([]);
     useEffect(() => {
-                loadSavedBooks();
+        loadSavedBooks();
     }, [])
     function loadSavedBooks() {
-                API.getBooks().then(res => {
-                    console.log(res);
-                    const bookArr = res.data.map((book,index) => {
-                        //attempt to check if image exists for saved book
-                        console.log(book);
-                        let thumbnail
-                        if(book.thumbnail){
-                           thumbnail =  <img src={book.thumbnail}></img>
-                        }
-                        return (
-                        <div className="card m-2">
-                            <div className="card-body col-6">
-                                {thumbnail}
-                                <h4>{book.title}</h4>
-                                <div>{book.authors}</div>
-                                <hr></hr> 
-                                <div>{book.description}</div>
-                                <a href={book.link}></a>
-                                <div className="btn btn-danger" onClick={()=>{API.deleteBook(book._id).then(()=>{loadSavedBooks()})}}>delete</div>
-                            </div>
+        API.getBooks().then(res => {
+            console.log(res);
+            const bookArr = res.data.map((book, index) => {
+                //attempt to check if image exists for saved book
+                console.log(book);
+                let thumbnail
+                if (book.thumbnail) {
+                    thumbnail = <img src={book.thumbnail}></img>
+                }
+                return (
+                    <div className="card m-2">
+                        <div className="card-body col-6">
+                            {thumbnail}
+                            <h4>{book.title}</h4>
+                            <div>{book.authors}</div>
+                            <hr></hr>
+                            <div>{book.description}</div>
+                            <a href={book.link}></a>
+                            <div className="btn btn-danger" onClick={() => { API.deleteBook(book._id).then(() => { loadSavedBooks() }) }}>delete</div>
                         </div>
-                        )
-                    })
-                    setBooks(bookArr);
+                    </div>
+                )
+            })
+            setBooks(bookArr);
 
-                })
-            }
+        })
+    }
 
     return (
-            <div>
-                {books}
+        <div>
+            {books}
 
 
-            </div>)
+        </div>)
 }
 
